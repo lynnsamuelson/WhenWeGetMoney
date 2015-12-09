541dd98d6f9fe84e8f3cdb19412d4dbce0d95113
lynnsamuelson <lynn.samuelson@gmail.com>
2015-12-09 08:57:23 -0600

    added the MoneyPot Model and initial tests

---
+++ WhenWeGetMoney/Models/MoneyPot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WhenWeGetMoney.Models
+{
+    public class MoneyPot
+    {
+        public decimal DollarAmount { get; set; }
+        [key]
+        public int MoneyPotId { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
--- WhenWeGetMoney/Models/Wish.cs
+++ WhenWeGetMoney/Models/Wish.cs
@@ -9,8 +9,11 @@ namespace WhenWeGetMoney.Models
     public class Wish
     {
         public object Author { get; set; }
+        [MaxLength(75)]
+        [MinLength(3)]
         public string Content { get; set; }
         public DateTime Date { get; set; }
+        [key]
         public int WishId { get; set; }
         public int WishPriority { get; set; }
         public string Picture { get; set; }
